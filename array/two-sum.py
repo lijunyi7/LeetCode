f2d993e5a6fc2dd96ad13dcd03b8d880f763137c
lijunyi7 <83321585+lijunyi7@users.noreply.github.com>
2025-06-25 00:20:09 -0700

    LC-two-sum [Runtime: 0 ms; Memory: 13.4 MB]

--- array/two-sum.py
+++ array/two-sum.py
@@ -5,10 +5,14 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(0, len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        hash_table = {}
+        for i in range(len(nums)):
+            hash_table[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hash_table and hash_table[complement] != i:
+                return [i, hash_table[complement]]
+            
 
 
         
