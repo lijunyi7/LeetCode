14f56041ae72b6ee1a50eafb9c1b5fb1e5d3fd71
lijunyi7 <83321585+lijunyi7@users.noreply.github.com>
2025-07-10 03:27:17 -0700

    LC-valid-sudoku [Runtime: N/A; Memory: N/A]

---
+++ matrix/valid-sudoku.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        #Create a hash set for col, row and boxes
+        # for later use
+        N = 9
+        rows = [set() for _ in range(N)]
+        cols = [set() for _ in range(N)]
+        boxes = [set() for _ in range(N)]
+
+        for row in range(N):
+            for col in range(N):
+                value = board[row][col]
+                if value == ".":
+                    continue
+                if value in rows[row]:
+                    return False
+                rows[row].add(value)
+                if value in cols[col]:
+                    return False
+                rows[col].add(value)
+                idx = (row//3)*3 + (col//3)
+                if value in boxes[idx]:
+                    return False
+                boxes[idx].add(value)
+
+        return True
+                
+        
