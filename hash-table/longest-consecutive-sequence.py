76c640996c2eb5f77e57be7e2bdbbd2d4194c1db
lijunyi7 <83321585+lijunyi7@users.noreply.github.com>
2025-07-16 04:55:20 -0700

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

---
+++ hash-table/longest-consecutive-sequence.py
@@ -0,0 +1,20 @@
+#clarifying question: what if the array is empty? what should I return?
+#
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums_sorted = sorted(set(nums))
+        prev = nums_sorted[0]
+        hashmap = {}
+        consective_term = 1
+        hashmap[consective_term] = [prev]
+        for num in nums_sorted[1::]:
+            if abs(num - prev) > 1:
+                consective_term += 1
+                prev = num
+                continue
+            elif abs(num - prev) == 1:
+                hashmap[consective_term].append(num)
+                prev = num
+        return max(len(v) for v in hashmap.values())
+
+                
