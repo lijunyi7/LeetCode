36657f1f77eacc5e0d7283933347ecaa3cda4885
lijunyi7 <83321585+lijunyi7@users.noreply.github.com>
2026-09-02 20:53:36 -0700

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

--- union-find/longest-consecutive-sequence.py
+++ union-find/longest-consecutive-sequence.py
@@ -4,14 +4,16 @@ class Solution:
         if not nums:
             return 0
         result = 1
+        longest = 1
         for i in range(1, len(sort_nums)):
             if sort_nums[i-1] + 1 == sort_nums[i]:
                 result += 1
+                longest = max(longest, result)
             elif sort_nums[i-1] == sort_nums[i]:
                 continue
             else:
                 result = 1
-        return result
+        return longest
                 
 
 
