67f476e0dda93d694c84c7eb2b20748affb9d4fa
lijunyi7 <83321585+lijunyi7@users.noreply.github.com>
2026-09-02 20:50:40 -0700

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

--- union-find/longest-consecutive-sequence.py
+++ union-find/longest-consecutive-sequence.py
@@ -1,15 +1,17 @@
-#clarifying question: what if the array is empty? what should I return?
-#
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        nums_sorted = set(nums)
-        longest_streak = 0
-        for num in nums_sorted:
-            if num - 1 not in nums_sorted:
-                curr_num = num
-                curr_streak = 1
-                while curr_num + 1 in nums_sorted:
-                    curr_streak += 1
-                    curr_num += 1
-                longest_streak = max(curr_streak, longest_streak)
-        return longest_streak
+        sort_nums = sorted(nums)
+        result = 1
+        for i in range(1, len(sort_nums)):
+            if sort_nums[i-1] + 1 == sort_nums[i]:
+                result += 1
+            elif sort_nums[i-1] == sort_nums[i]:
+                continue
+            else:
+                reuslt = 1
+        return result
+                
+
+
+            
+        
