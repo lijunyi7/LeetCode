af2fc0843138581b491df13a0f0605c55203cd72
lijunyi7 <83321585+lijunyi7@users.noreply.github.com>
2026-09-04 18:44:20 -0700

    LC-two-sum-ii-input-array-is-sorted [Runtime: N/A; Memory: N/A]

--- array/two-sum-ii-input-array-is-sorted.py
+++ array/two-sum-ii-input-array-is-sorted.py
@@ -1,31 +1,16 @@
-"clarifying question: what if the input arry is empty, what shoudl I return "
-"What if there is only one number"
-"What if there is no number that sum to the traget"
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        """
-        Initial thought: 
-        using two pinters, one at the begaining of the array and one at the end
-        add them together, compare the result with target, if target is smaller then the sum, move the left pointer to right and vice versa
-        """
-        if len(numbers) <= 1:
+        if not numbers:
             return []
-        right_index = 0
-        left_index = -1
-        while right_index != left_index + len(numbers):
-            curr_sum = numbers[right_index] + numbers[left_index]
-            if curr_sum == target:
-                return [right_index + 1, left_index + + len(numbers) + 1]
-            elif curr_sum < target:
-                right_index += 1
+        right = 0
+        left = len(numbers) - 1
+        while(right < left):
+            if numbers[right] + numbers[left] == target:
+                return [right + 1, left + 1]
+            elif numbers[right] + numbers[left] > target:
+                left -= 1
             else:
-                left_index -= 1
-        return []
-
-           
-
-
-               
-        
+                right += 1
+            
 
         
