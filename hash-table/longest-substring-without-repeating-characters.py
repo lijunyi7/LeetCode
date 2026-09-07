c5d5c72dd1b255dfe1ba416b2bb54e4da8552556
lijunyi7 <83321585+lijunyi7@users.noreply.github.com>
2026-09-06 19:24:55 -0700

    LC-longest-substring-without-repeating-characters [Runtime: N/A; Memory: N/A]

--- hash-table/longest-substring-without-repeating-characters.py
+++ hash-table/longest-substring-without-repeating-characters.py
@@ -1,6 +1,6 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        seen = ()
+        seen = set()
         longest_len = 0
         current_win_len = 0
         left = 0
@@ -10,7 +10,7 @@ class Solution:
                 left += 1
             seen.add(s[right])
             right += 1
-            longest_len = max(longest_len, right - left + 1)
+            longest_len = max(longest_len, right - left)
         return longest_len
             
             
