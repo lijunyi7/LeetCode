2d4950b20ad71622b3383178a2d164b31bed2fa9
lijunyi7 <83321585+lijunyi7@users.noreply.github.com>
2026-09-09 19:24:20 -0700

    LC-longest-repeating-character-replacement [Runtime: N/A; Memory: N/A]

---
+++ sliding-window/longest-repeating-character-replacement.py
@@ -0,0 +1,19 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        freq_tracker = {}
+        left = 0
+        right = 0
+        max_freq = 0
+        result = 0
+        for right in range(len(s)):
+            freq_tracker[s[right]] = freq_tracker.get(s[right], 0) + 1
+            max_freq = max(max_freq, freq_tracker[s[right]])
+            while((right - left + 1) - max_freq > k):
+                freq_tracker[s[left]] -= 1
+                left += 1
+            result = max(result, max_freq + k)
+        return result
+
+
+                
+        
